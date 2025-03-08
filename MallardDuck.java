eb438f533d4f38176365b2446357ce54c2eac537
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-08 18:47:39 +0530

    Create MallardDuck.java

---
+++ MallardDuck.java
@@ -0,0 +1,12 @@
+public class MallardDuck extends Duck {
+    public MallardDuck() {
+        quackBehaviour = new Quack();
+        swimBehaviour = new SwimWithLegs();
+        flyBehaviour = new Fly();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am Mallard Duck.");
+    }
+}
