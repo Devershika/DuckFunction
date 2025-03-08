3a261f2b93b328baca79b9823c0e320ad6863903
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-08 18:43:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,3 +16,27 @@ public class Main {
         redHeadDuck.performSwim();
         redHeadDuck.performFly();
         System.out.println("__________________");
+
+    
+        System.out.println("__________________");
+        mallardDuck.display();
+        mallardDuck.performQuack();
+        mallardDuck.performSwim();
+        mallardDuck.performFly();
+        System.out.println("__________________");
+
+        System.out.println("__________________");
+        rubberDuck.display();
+        rubberDuck.performQuack();
+        rubberDuck.performSwim();
+        rubberDuck.performFly();
+        System.out.println("__________________");
+
+        System.out.println("__________________");
+        drowningDuck.display();
+        drowningDuck.performQuack();
+        drowningDuck.performSwim(); // Now drowns instead of floating
+        drowningDuck.performFly();
+        System.out.println("__________________");
+    }
+}
