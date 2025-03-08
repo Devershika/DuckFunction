c4a6415f37c68f1aba17f39f8bc378731d9c1c6f
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-08 18:34:35 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
+//DEVERSHIKA MOHANE
+//23070126032
+//AIML A2
 
+public class Main {
+    public static void main(String[] args) {
+        Duck mallardDuck = new MallardDuck();
+        Duck redHeadDuck = new RedHeadDuck();
+        Duck rubberDuck = new RubberDuck();
+        Duck drowningDuck = new RubberDuck(); // Simulating a duck that drowns
+        drowningDuck.setSwimBehaviour(new Drown());
+
+        System.out.println("__________________");
+        redHeadDuck.display();
+        redHeadDuck.performQuack();
+        redHeadDuck.performSwim();
+        redHeadDuck.performFly();
+        System.out.println("__________________");
